fc823fb17ce67240a238ab968a28eff658726cc1
Florian Löhden <florian.loehden@capgemini.com>
2019-09-04 15:00:05 +0200

    Updated NPM and Gradle Reader
    Added NPM and Gradle Reader and added some Ascii Doc Documentation for it

---
+++ src/main/java/com/devonfw/tools/solicitor/reader/gradle/model/License.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2019 Capgemini SE.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.devonfw.tools.solicitor.reader.gradle.model;
+
+import lombok.Data;
+
+@Data
+public class License {
+    private String license;
+    private String license_url;
+    private String distribution;
+
+    @Override
+    public String toString() {
+    	return "";
+    }
+}
