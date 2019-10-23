dd272c5c483109319273b01d62e4259295afdae7
Oliver Hecker <oliver.hecker@capgemini.com>
2019-10-23 20:48:50 +0200

    Introduce Builder for Copy Operations

--- src/main/java/com/devonfw/tools/solicitor/common/ResourceToFileCopier.java
+++ src/main/java/com/devonfw/tools/solicitor/common/ResourceToFileCopier.java
@@ -8,6 +8,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +23,125 @@ import org.springframework.stereotype.Component;
 @Component
 public class ResourceToFileCopier {
     private static final Logger LOG = LoggerFactory.getLogger(ResourceToFileCopier.class);
+
+    /**
+     * Represents a single copy operation to be performed
+     */
+    private static class CopyOperation {
+        private String source;
+
+        private String target;
+
+        /**
+         * The Constructor.
+         * 
+         * @param source the URL of the source to copy from
+         * @param target the target (filename)
+         */
+        public CopyOperation(String source, String target) {
+
+            this.source = source;
+            this.target = target;
+        }
+
+        /**
+         * This method gets the field <tt>source</tt>.
+         *
+         * @return the field source
+         */
+        public String getSource() {
+
+            return source;
+        }
+
+        /**
+         * This method sets the field <tt>source</tt>.
+         *
+         * @param source the new value of the field source
+         */
+        public void setSource(String source) {
+
+            this.source = source;
+        }
+
+        /**
+         * This method gets the field <tt>target</tt>.
+         *
+         * @return the field target
+         */
+        public String getTarget() {
+
+            return target;
+        }
+
+        /**
+         * This method sets the field <tt>target</tt>.
+         *
+         * @param target the new value of the field target
+         */
+        public void setTarget(String target) {
+
+            this.target = target;
+        }
+
+    }
+
+    /**
+     * A Builder for creating a sequence of copy operations.
+     */
+    private class CopySequenceBuilder {
+        private List<CopyOperation> operations;
+
+        /**
+         * The constructor.
+         */
+        public CopySequenceBuilder() {
+
+            operations = new ArrayList<>();
+        }
+
+        /**
+         * Schedule a copy operation.
+         * 
+         * @param source the source to copy from (URL)
+         * @param target the target to copy to (filename)
+         * @return the builder (to allow method chaining)
+         */
+        public CopySequenceBuilder withCopyOperation(String source, String target) {
+
+            operations.add(new CopyOperation(source, target));
+            return this;
+        }
+
+        /**
+         * Execute pattern replacement in all target filenames of the scheduled
+         * copy operations. This uses {@link String#replaceAll(String, String)}.
+         * 
+         * @param pattern the pattern to replace (regex)
+         * @param replacement the replacement for the pattern; observe that some
+         *        characters have special meaning due to regex functionality
+         * @return the builder (to allow method chaining)
+         * @see String#replaceAll(String, String)
+         * @see Matcher#replaceAll(String)
+         */
+        public CopySequenceBuilder replaceInTarget(String pattern, String replacement) {
+
+            for (CopyOperation operation : operations) {
+                operation.setTarget(operation.getTarget().replaceAll(pattern, replacement));
+            }
+            return this;
+        }
+
+        /**
+         * Execute the scheduled copy operations.
+         */
+        public void execute() {
+
+            for (CopyOperation operation : operations) {
+                copyResourceToFile(operation.getSource(), operation.getTarget());
+            }
+        }
+    }
 
     /**
      * Depicts different groups of resources which might be copied.
@@ -65,50 +187,56 @@ public class ResourceToFileCopier {
 
         switch (resourceGroup) {
         case USERGUIDE:
-            copyResourceToFile("classpath:solicitor_userguide.pdf", "solicitor_userguide.pdf");
+            new CopySequenceBuilder().withCopyOperation("classpath:solicitor_userguide.pdf", "solicitor_userguide.pdf")
+                    .execute();
             returnString = "solicitor_userguide.pdf";
             break;
         case PROJECT_FILES:
-            copyResourceToFile("classpath:starters/solicitor.cfg", "new_project/solicitor.cfg");
-            copyResourceToFile("classpath:starters/input/licenses_starter.xml",
-                    "new_project/input/licenses_starter.xml");
-            copyResourceToFile("classpath:starters/rules/LegalEvaluationProject.xls",
-                    "new_project/rules/LegalEvaluationProject.xls");
-            copyResourceToFile("classpath:starters/rules/LegalPreEvaluationProject.xls",
-                    "new_project/rules/LegalPreEvaluationProject.xls");
-            copyResourceToFile("classpath:starters/rules/LicenseAssignmentProject.xls",
-                    "new_project/rules/LicenseAssignmentProject.xls");
-            copyResourceToFile("classpath:starters/rules/LicenseNameMappingProject.xls",
-                    "new_project/rules/LicenseNameMappingProject.xls");
-            copyResourceToFile("classpath:starters/rules/LicenseSelectionProject.xls",
-                    "new_project/rules/LicenseSelectionProject.xls");
-            copyResourceToFile("classpath:starters/rules/MultiLicenseSelectionProject.xls",
-                    "new_project/rules/MultiLicenseSelectionProject.xls");
-            copyResourceToFile("classpath:starters/readme.txt", "new_project/readme.txt");
+            new CopySequenceBuilder().withCopyOperation("classpath:starters/solicitor.cfg", "new_project/solicitor.cfg")
+                    .withCopyOperation("classpath:starters/input/licenses_starter.xml",
+                            "new_project/input/licenses_starter.xml")
+                    .withCopyOperation("classpath:starters/rules/LegalEvaluationProject.xls",
+                            "new_project/rules/LegalEvaluationProject.xls")
+                    .withCopyOperation("classpath:starters/rules/LegalPreEvaluationProject.xls",
+                            "new_project/rules/LegalPreEvaluationProject.xls")
+                    .withCopyOperation("classpath:starters/rules/LicenseAssignmentProject.xls",
+                            "new_project/rules/LicenseAssignmentProject.xls")
+                    .withCopyOperation("classpath:starters/rules/LicenseNameMappingProject.xls",
+                            "new_project/rules/LicenseNameMappingProject.xls")
+                    .withCopyOperation("classpath:starters/rules/LicenseSelectionProject.xls",
+                            "new_project/rules/LicenseSelectionProject.xls")
+                    .withCopyOperation("classpath:starters/rules/MultiLicenseSelectionProject.xls",
+                            "new_project/rules/MultiLicenseSelectionProject.xls")
+                    .withCopyOperation("classpath:starters/readme.txt", "new_project/readme.txt").execute();
             returnString = "new_project/readme.txt";
             break;
         case CONFIG_FULL:
-            copyResourceToFile("classpath:samples/solicitor_sample_filesystem.cfg", "solicitor_sample_copy.cfg");
-            copyResourceToFile("classpath:samples/licenses_devon4j.xml", "licenses_devon4j.xml");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/rules/LicenseAssignmentSample.xls",
-                    "sample_configs/LicenseAssignmentSample.xls");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/rules/LicenseNameMappingSample.xls",
-                    "sample_configs/LicenseNameMappingSample.xls");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/rules/MultiLicenseSelectionSample.xls",
-                    "sample_configs/MultiLicenseSelectionSample.xls");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/rules/LicenseSelectionSample.xls",
-                    "sample_configs/LicenseSelectionSample.xls");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/rules/LegalPreEvaluationSample.xls",
-                    "sample_configs/LegalPreEvaluationSample.xls");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/rules/LegalEvaluationSample.xls",
-                    "sample_configs/LegalEvaluationSample.xls");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/templates/Solicitor_Output_Template_Sample.vm",
-                    "sample_configs/Solicitor_Output_Template_Sample.vm");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/templates/Solicitor_Output_Template_Sample.xlsx",
-                    "sample_configs/Solicitor_Output_Template_Sample.xlsx");
-            copyResourceToFile("classpath:com/devonfw/tools/solicitor/templates/Solicitor_Diff_Template_Sample.vm",
-                    "sample_configs/Solicitor_Diff_Template_Sample.vm");
-            copyResourceToFile("classpath:samples/readme_solicitor_cfg.txt", "readme_solicitor_cfg.txt");
+            new CopySequenceBuilder()
+                    .withCopyOperation("classpath:samples/solicitor_sample_filesystem.cfg", "solicitor_sample_copy.cfg")
+                    .withCopyOperation("classpath:samples/licenses_devon4j.xml", "licenses_devon4j.xml")
+                    .withCopyOperation("classpath:com/devonfw/tools/solicitor/rules/LicenseAssignmentSample.xls",
+                            "sample_configs/LicenseAssignmentSample.xls")
+                    .withCopyOperation("classpath:com/devonfw/tools/solicitor/rules/LicenseNameMappingSample.xls",
+                            "sample_configs/LicenseNameMappingSample.xls")
+                    .withCopyOperation("classpath:com/devonfw/tools/solicitor/rules/MultiLicenseSelectionSample.xls",
+                            "sample_configs/MultiLicenseSelectionSample.xls")
+                    .withCopyOperation("classpath:com/devonfw/tools/solicitor/rules/LicenseSelectionSample.xls",
+                            "sample_configs/LicenseSelectionSample.xls")
+                    .withCopyOperation("classpath:com/devonfw/tools/solicitor/rules/LegalPreEvaluationSample.xls",
+                            "sample_configs/LegalPreEvaluationSample.xls")
+                    .withCopyOperation("classpath:com/devonfw/tools/solicitor/rules/LegalEvaluationSample.xls",
+                            "sample_configs/LegalEvaluationSample.xls")
+                    .withCopyOperation(
+                            "classpath:com/devonfw/tools/solicitor/templates/Solicitor_Output_Template_Sample.vm",
+                            "sample_configs/Solicitor_Output_Template_Sample.vm")
+                    .withCopyOperation(
+                            "classpath:com/devonfw/tools/solicitor/templates/Solicitor_Output_Template_Sample.xlsx",
+                            "sample_configs/Solicitor_Output_Template_Sample.xlsx")
+                    .withCopyOperation(
+                            "classpath:com/devonfw/tools/solicitor/templates/Solicitor_Diff_Template_Sample.vm",
+                            "sample_configs/Solicitor_Diff_Template_Sample.vm")
+                    .withCopyOperation("classpath:samples/readme_solicitor_cfg.txt", "readme_solicitor_cfg.txt")
+                    .execute();
             returnString = "readme_solicitor_cfg.txt";
             break;
         default:
@@ -119,7 +247,7 @@ public class ResourceToFileCopier {
     }
 
     /**
-     * Copies a single reource to the file system.
+     * Copies a single resource to the file system.
      *
      * @param resourceUrl the URL of the resource to be used as source
      * @param fileName the name of the target file.
