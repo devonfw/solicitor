92d4754df7bf7338744ba07ca2f8f77f7e6c54c5
ohecker <oliver.hecker@capgemini.com>
2021-10-01 11:28:54 +0200

    Prepare to support multiple versions of JSON model files

---
+++ src/test/java/com/devonfw/tools/solicitor/model/ModelImporterExporterTest.java
@@ -0,0 +1,60 @@
+package com.devonfw.tools.solicitor.model;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.devonfw.tools.solicitor.common.SolicitorRuntimeException;
+
+/**
+ * Tests of {@link ModelImporterExporter}.
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelImporterExporterTest {
+
+  @Autowired
+  private ModelImporterExporter mie;
+
+  /**
+   * Test method for {@link com.devonfw.tools.solicitor.model.ModelImporterExporter#loadModel(java.lang.String)}.
+   */
+  @Test(expected = SolicitorRuntimeException.class)
+  public void testLoadModelVersion1() {
+
+    this.mie.loadModel("src/test/resources/models/model_version_1.json");
+  }
+
+  /**
+   * Test method for {@link com.devonfw.tools.solicitor.model.ModelImporterExporter#loadModel(java.lang.String)}.
+   */
+  @Test
+  public void testLoadModelVersion2() {
+
+    this.mie.loadModel("src/test/resources/models/model_version_2.json");
+  }
+
+  /**
+   * Test method for
+   * {@link com.devonfw.tools.solicitor.model.ModelImporterExporter#saveModel(com.devonfw.tools.solicitor.model.ModelRoot, java.lang.String)}.
+   *
+   * @throws IOException in case that the temp file could not be created
+   */
+  @Test
+  public void testSaveModel() throws IOException {
+
+    ModelRoot mr = this.mie.loadModel("src/test/resources/models/model_version_2.json");
+    File tempFile = File.createTempFile("soluicitor_model", "json");
+    String fileName = tempFile.getPath();
+
+    this.mie.saveModel(mr, fileName);
+    this.mie.loadModel(fileName);
+  }
+
+}
