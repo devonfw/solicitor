4e99dc9217cf62982e9e3a68becd2112304451a2
Oliver Hecker <oliver.hecker@capgemini.com>
2021-10-31 01:01:53 +0200

    Improve license url guessing (#93)
    * Handle cases where branch name is main instead of master and where Github project URL ends with a slash
    
    * Improved license guessing strategy

--- src/main/java/com/devonfw/tools/solicitor/licensetexts/StrategyLicenseUrlGuesser.java
+++ src/main/java/com/devonfw/tools/solicitor/licensetexts/StrategyLicenseUrlGuesser.java
@@ -3,128 +3,157 @@
  */
 package com.devonfw.tools.solicitor.licensetexts;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.devonfw.tools.solicitor.SolicitorVersion;
 import com.devonfw.tools.solicitor.common.content.ContentProvider;
 import com.devonfw.tools.solicitor.common.content.web.WebContent;
 
 /**
- * A {@link LicenseUrlGuesser} which tries to strategically find a possible better license URL.
+ * A {@link LicenseUrlGuesser} which tries to strategically find a possible
+ * better license URL.
  */
 public class StrategyLicenseUrlGuesser implements LicenseUrlGuesser {
 
-  private static final Logger LOG = LoggerFactory.getLogger(StrategyLicenseUrlGuesser.class);
+    private static final Logger LOG = LoggerFactory.getLogger(StrategyLicenseUrlGuesser.class);
 
-  private ContentProvider<WebContent> webContentProvider;
+    private ContentProvider<WebContent> webContentProvider;
 
-  /**
-   * The constructor.
-   *
-   * @param webContentProvider the provider for retrieving the content of URLs
-   */
-  public StrategyLicenseUrlGuesser(ContentProvider<WebContent> webContentProvider) {
+    private SolicitorVersion solicitorVersion;
 
-    this.webContentProvider = webContentProvider;
-  }
+    /**
+     * The constructor.
+     *
+     * @param webContentProvider the provider for retrieving the content of URLs
+     * @param solicitorVersion version info to be used in audit info
+     */
+    public StrategyLicenseUrlGuesser(ContentProvider<WebContent> webContentProvider,
+            SolicitorVersion solicitorVersion) {
 
-  // helper method that returns true if the given url is available and false if the given url is a 404 not found page
-  private boolean pingURL(String url, StringBuilder traceBuilder) {
-
-    WebContent content = this.webContentProvider.getContentForUri(url);
-    if (content != null && content.getContent() != null && !content.getContent().isEmpty()) {
-      setTrace("Found license." + "\n", traceBuilder);
-      return true;
-    } else {
-      setTrace("Nothing found" + "\n", traceBuilder);
-      return false;
+        this.webContentProvider = webContentProvider;
+        this.solicitorVersion = solicitorVersion;
     }
-  }
 
-  private void setTrace(String trace, StringBuilder traceBuilder) {
+    // helper method that returns true if the given url is available and false
+    // if the given url is a 404 not found page
+    private boolean pingURL(String url, StringBuilder traceBuilder) {
 
-    traceBuilder.append(trace).append('\n');
-  }
-
-  // https://github.com/nodelib/nodelib/tree/master/packages/fs/fs.stat
-  // https://raw.githubusercontent.com/nodelib/nodelib/master/packages/fs/fs.stat/README.md
-  // helper method that normalizes a github url and retrieves the raw link to a given license
-  private String normalizeGitURL(String url, StringBuilder traceBuilder) {
-
-    String oldURL = url;
-    if (url.contains("github.com") && url.contains("raw/")) {
-      // case that declared license points to old raw link github license file; change it to new
-      url = url.replace("github.com", "raw.githubusercontent.com");
-      url = url.replace("raw/", "");
-      setTrace("URL changed from " + oldURL + " to " + url, traceBuilder);
-      return url;
-    } else if (url.contains("github.com") && url.contains("blob/")) {
-      // case that declared license points to github non raw license file; change it to raw
-      url = url.replace("github.com", "raw.githubusercontent.com");
-      url = url.replace("blob/", "");
-      setTrace("URL changed from " + oldURL + " to " + url, traceBuilder);
-      return url;
-    } else if (url.contains("github.com") && url.contains("tree/")) {
-      // case that declared license points to github non raw license file; change it to raw
-      url = url.replace("tree/", "");
-      setTrace("URL changed from " + oldURL + " to " + url, traceBuilder);
-      return normalizeGitURL(url, traceBuilder);
-    } else if (url.contains("github.com") && !(url.contains("blob/")) && !(url.contains("tree/"))) {
-      // case that declared license points to main github page but not file
-      // try different variations of license names in the mainfolder / take readme if non existent
-      String[] licensefilenames = { "LICENSE", "License", "license", "LICENSE.md", "LICENSE.txt", "license.html",
-      "license.txt", "COPYING", "LICENSE-MIT", "LICENSE-MIT.txt", "README.md", "Readme.md", "readme.md",
-      "README.markdown" };
-      url = url.replace("github.com", "raw.githubusercontent.com");
-      for (String name : licensefilenames) {
-        String testURL;
-        if (!url.contains("/master/")) {
-          testURL = url.concat("/master/" + name);
+        WebContent content = this.webContentProvider.getContentForUri(url);
+        if (content != null && content.getContent() != null && !content.getContent().isEmpty()) {
+            setTrace("Found content." + "\n", traceBuilder);
+            return true;
         } else {
-          testURL = url.concat("/" + name);
+            setTrace("Nothing found" + "\n", traceBuilder);
+            return false;
         }
-        setTrace("Searching for license: testing with " + testURL, traceBuilder);
-        if (pingURL(testURL, traceBuilder) == true) {
-          setTrace("URL changed from " + oldURL + " to " + testURL, traceBuilder);
-          return normalizeGitURL(testURL, traceBuilder);
-        }
-      }
     }
-    return oldURL;
-  }
 
-  // // helper method that normalizes a readme file; cuts off overhead and preserves license text
-  // private String normalizeReadMe(String readme) {
-  //
-  // int licenseIndex = readme.toLowerCase().indexOf("License");
-  // if (licenseIndex != -1) {
-  // return readme.substring(licenseIndex);
-  // } else {
-  // return "Could not retrieve license information in this repository (License file/Readme) \n Please check manually.";
-  // }
-  // }
+    private void setTrace(String trace, StringBuilder traceBuilder) {
 
-  // @Override
-  // public WebContentObject getWebContentForUrl(WebContentObject webObj) {
-  //
-  // String url = webObj.getUrl();
-  // webObj.setEffectiveURL(normalizeGitURL(url));
-  // WebContentObject copyObj = this.directWebContentProvider.getWebContentForUrl(webObj);
-  // webObj.setContent(copyObj.getContent());
-  // // checks if a readme url contains the keyword "license" and cuts out overhead
-  // if (webObj.getEffectiveURL().contains("README.md")) {
-  // webObj.setContent(normalizeReadMe(webObj.getContent()));
-  // }
-  // webObj.setTrace(this.trace);
-  // return webObj;
-  // }
+        traceBuilder.append(trace).append('\n');
+    }
 
-  @Override
-  public GuessedLicenseUrlContent getContentForUri(String uri) {
+    // https://github.com/nodelib/nodelib/tree/master/packages/fs/fs.stat
+    // https://raw.githubusercontent.com/nodelib/nodelib/master/packages/fs/fs.stat/README.md
+    // helper method that normalizes a github url and retrieves the raw link to
+    // a given license
+    private String normalizeGitURL(String url, StringBuilder traceBuilder) {
 
-    StringBuilder auditLogBuilder = new StringBuilder();
-    String guessedUrl = normalizeGitURL(uri, auditLogBuilder);
-    return new GuessedLicenseUrlContent(guessedUrl, auditLogBuilder.toString());
-  }
+        String oldURL = url;
+        if (url.contains("github")) {
+            // use https for all github URLs
+            url = url.replace("http:", "https:");
+            // omit repo suffix if existent
+            url = url.replace(".git", "");
+        }
+        if (url.contains("github.com") && url.contains("raw/")) {
+            // case that declared license points to old raw link github license
+            // file; change it to new
+            url = url.replace("github.com", "raw.githubusercontent.com");
+            url = url.replace("raw/", "");
+            setTrace("URL changed from " + oldURL + " to " + url, traceBuilder);
+            return url;
+        } else if (url.contains("github.com") && url.contains("blob/")) {
+            // case that declared license points to github non raw license file;
+            // change it to raw
+            url = url.replace("github.com", "raw.githubusercontent.com");
+            url = url.replace("blob/", "");
+            setTrace("URL changed from " + oldURL + " to " + url, traceBuilder);
+            return url;
+        } else if (url.contains("github.com") && url.contains("tree/")) {
+            // case that declared license points to github non raw license file;
+            // change it to raw
+            url = url.replace("tree/", "");
+            setTrace("URL changed from " + oldURL + " to " + url, traceBuilder);
+            return normalizeGitURL(url, traceBuilder);
+        } else if (url.contains("github.com") && !(url.contains("blob/")) && !(url.contains("tree/"))) {
+            // case that declared license points to main github page but not
+            // file
+            // try different variations of license names in the mainfolder /
+            // take readme if non existent
+            String[] licensefilenames = { "LICENSE", "License", "license", "LICENSE.md", "LICENSE.txt", "license.html",
+            "license.txt", "COPYING", "LICENSE-MIT", "LICENSE-MIT.txt", "README.md", "Readme.md", "readme.md",
+            "README.markdown" };
+            url = url.replace("github.com", "raw.githubusercontent.com");
+            if (url.endsWith("/")) {
+                url = url.substring(0, url.length() - 1);
+            }
+            for (String name : licensefilenames) {
+                String testURL;
+                if (url.contains("/master/") || url.contains("/main/")) {
+                    testURL = url.concat("/" + name);
+                } else {
+                    testURL = url.concat("/master/" + name);
+                }
+                setTrace("Searching for license: testing with " + testURL, traceBuilder);
+                if (pingURL(testURL, traceBuilder) == true) {
+                    setTrace("URL changed from " + oldURL + " to " + testURL, traceBuilder);
+//                    return normalizeGitURL(testURL, traceBuilder);
+                    return testURL;
+                }
+                if (testURL.contains("master")) {
+                    testURL = testURL.replace("master", "main");
+                    setTrace("Searching for license: testing with " + testURL, traceBuilder);
+                    if (pingURL(testURL, traceBuilder) == true) {
+                        setTrace("URL changed from " + oldURL + " to " + testURL, traceBuilder);
+//                        return normalizeGitURL(testURL, traceBuilder);
+                        return testURL;
+
+                    }
+                }
+            }
+        }
+        if (url.contains("http:")) {
+            String testURL = url.replace("http:", "https:");
+            setTrace("Searching for license: testing with " + testURL, traceBuilder);
+            if (pingURL(testURL, traceBuilder) == true) {
+                setTrace("URL changed from " + oldURL + " to " + testURL, traceBuilder);
+                return testURL;
+            }
+        }
+        return oldURL;
+    }
+
+    @Override
+    public GuessedLicenseUrlContent getContentForUri(String uri) {
+
+        StringBuilder auditLogBuilder = new StringBuilder();
+        String now = ZonedDateTime.now().truncatedTo(ChronoUnit.SECONDS).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        setTrace("(URL guessed by Solicitor " + this.solicitorVersion.getVersion() + " ["
+                + this.solicitorVersion.getGithash() + "], " + now + " / unconfirmed)\n", auditLogBuilder);
+        String guessedUrl = normalizeGitURL(uri, auditLogBuilder);
+        if (uri != null && uri.equals(guessedUrl)) {
+            setTrace("Original URL remains unchanged.\n", auditLogBuilder);
+        }
+        setTrace("Resulting URL: " + guessedUrl, auditLogBuilder);
+        pingURL(guessedUrl, auditLogBuilder);
+
+        return new GuessedLicenseUrlContent(guessedUrl, auditLogBuilder.toString());
+    }
 
 }
