6f2d247e1b2e89b940482e960aafa621e80f1aba
ohecker <8004361+ohecker@users.noreply.github.com>
2022-02-13 21:37:45 +0100

    Some cleanup in Review

--- core/src/main/java/com/devonfw/tools/solicitor/ruleengine/drools/ModelHelper.java
+++ core/src/main/java/com/devonfw/tools/solicitor/ruleengine/drools/ModelHelper.java
@@ -20,234 +20,223 @@ import com.devonfw.tools.solicitor.model.masterdata.EngagementType;
 import com.devonfw.tools.solicitor.model.masterdata.GoToMarketModel;
 
 /**
- * Utility class for dealing with the data model.
+ * Utility class for dealing with the data model. This is used/initialized as Spring bean, but static methods are also
+ * used directly.
  */
 @Component
 public class ModelHelper {
-	
-	private static DeprecationChecker deprecationChecker;
 
-    @Autowired
-    public static void setDeprecationChecker(DeprecationChecker deprecationChecker) {
+  /**
+   * Prefix which marks a string as defining a regular expression condition.
+   */
+  private static final String REGEX_PREFIX = "REGEX:";
 
-        ModelHelper.deprecationChecker = deprecationChecker;
+  /**
+   * Suffix which marks a string as defining a regular expression condition.
+   */
+  private static final String REGEX_SUFFIX = "(REGEX)";
+
+  /**
+   * Prefix which marks a string as defining a maven range condition.
+   */
+  private static final String RANGE_PREFIX = "RANGE:";
+
+  /**
+   * Prefix which marks a logical negation on the rest of the condition.
+   */
+  private static final String NOT_PREFIX = "NOT:";
+
+  private static final Logger LOG = LoggerFactory.getLogger(ModelHelper.class);
+
+  private static DeprecationChecker deprecationChecker;
+
+  private static ModelFactory modelFactory;
+
+  private static String currentRuleGroup;
+
+  /**
+   * Saves a reference to the {@link DeprecationChecker} in a static variable.
+   *
+   * @param deprecationChecker a {@link DeprecationChecker} object.
+   */
+  @Autowired
+  public void setDeprecationChecker(DeprecationChecker deprecationChecker) {
+
+    ModelHelper.deprecationChecker = deprecationChecker;
+  }
+
+  /**
+   * Saves a reference to the ModelFactory in a static variable to allow access to factory methods via static methods.
+   *
+   * @param modelFactory a {@link ModelFactory} object.
+   */
+  @Autowired
+  public void setModelFactory(ModelFactory modelFactory) {
+
+    ModelHelper.modelFactory = modelFactory;
+  }
+
+  /**
+   * Adds the given comment to the comments of the given license object.
+   *
+   * @param license a {@link NormalizedLicense} object.
+   * @param comment the comment to append
+   */
+  public static void addCommentToNormalizedLicense(NormalizedLicense license, String comment) {
+
+    String comments = license.getComments();
+
+    if (comments == null) {
+      comments = comment;
+    } else {
+      comments = comments + "; " + comment;
     }
-	
-    /**
-     * Prefix which marks a string as defining a regular expression condition.
-     */
-    private static final String REGEX_PREFIX = "REGEX:";
+    license.setComments(comments);
+  }
 
-    /**
-     * Suffix which marks a string as defining a regular expression condition.
-     */
-    private static final String REGEX_SUFFIX = "(REGEX)";
-    
-    /**
-     * Prefix which marks a string as defining a maven range condition.
-     */
-    private static final String RANGE_PREFIX = "RANGE:";
+  /**
+   * Adds the given trace entry to the trace infos of the given license object.
+   *
+   * @param license a {@link NormalizedLicense} object.
+   * @param traceEntry the trace entry to be added to the trace field
+   */
+  public static void appendTraceToNormalizedLicense(NormalizedLicense license, String traceEntry) {
 
-    /**
-     * Prefix which marks a logical negation on the rest of the condition.
-     */
-    private static final String NOT_PREFIX = "NOT:";
-
-    private static final Logger LOG = LoggerFactory.getLogger(ModelHelper.class);
-
-    private static ModelFactory modelFactory;
-
-    private static String currentRuleGroup;
-
-    /**
-     * Adds the given comment to the comments of the given license object.
-     *
-     * @param license a {@link NormalizedLicense} object.
-     * @param comment the comment to append
-     */
-    public static void addCommentToNormalizedLicense(NormalizedLicense license, String comment) {
-
-        String comments = license.getComments();
-
-        if (comments == null) {
-            comments = comment;
-        } else {
-            comments = comments + "; " + comment;
-        }
-        license.setComments(comments);
+    LOG.debug(traceEntry);
+    String trace = license.getTrace();
+    if (trace == null) {
+      trace = traceEntry;
+    } else {
+      trace += System.lineSeparator() + traceEntry;
     }
+    license.setTrace(trace);
+  }
 
-    /**
-     * Adds the given trace entry to the trace infos of the given license
-     * object.
-     *
-     * @param license a {@link NormalizedLicense} object.
-     * @param traceEntry the trace entry to be added to the trace field
-     */
-    public static void appendTraceToNormalizedLicense(NormalizedLicense license, String traceEntry) {
+  /**
+   * Creates a new {@link Application} by calling
+   * {@link ModelFactory#newApplication(String, String, String, String, String)}.
+   *
+   * @param name the application name
+   * @param releaseId the release id.
+   * @param releaseDate the date of the release
+   * @param sourceRepo pointer to the source repo
+   * @param programmingEcosystem name of the programming ecosystem
+   * @return the new instance
+   */
+  public static Application newApplication(String name, String releaseId, String releaseDate, String sourceRepo,
+      String programmingEcosystem) {
 
-        LOG.debug(traceEntry);
-        String trace = license.getTrace();
-        if (trace == null) {
-            trace = traceEntry;
-        } else {
-            trace += System.lineSeparator() + traceEntry;
-        }
-        license.setTrace(trace);
+    return modelFactory.newApplication(name, releaseId, releaseDate, sourceRepo, programmingEcosystem);
+  }
+
+  /**
+   * Creates a new {@link ApplicationComponent} by calling {@link ModelFactory#newApplicationComponent()}.
+   *
+   * @return the new instance
+   */
+  public static ApplicationComponent newApplicationComponent() {
+
+    return modelFactory.newApplicationComponent();
+  }
+
+  /**
+   * Creates a new {@link Engagement} by calling
+   * {@link ModelFactory#newEngagement(String, EngagementType, String, GoToMarketModel)} .
+   *
+   * @param engagementName the name of the engagement
+   * @param engagementType the type of engagement
+   * @param clientName name of the client
+   * @param goToMarketModel the model how this goes to market
+   * @return the new instance
+   */
+  public static Engagement newEngagement(String engagementName, EngagementType engagementType, String clientName,
+      GoToMarketModel goToMarketModel) {
+
+    return modelFactory.newEngagement(engagementName, engagementType, clientName, goToMarketModel);
+  }
+
+  /**
+   * Creates a {@link NormalizedLicense} by calling {@link ModelFactory#newNormalizedLicense(RawLicense)}.
+   *
+   * @param rawLicense {@link RawLicense} from which to copy data
+   * @return the new instance
+   */
+  public static NormalizedLicense newNormalizedLicense(RawLicense rawLicense) {
+
+    return modelFactory.newNormalizedLicense(rawLicense);
+  }
+
+  /**
+   * Creates a new {@link RawLicense} by calling {@link ModelFactory#newRawLicense()} on the implementation instance.
+   *
+   * @return the new instance
+   */
+  public static RawLicense newRawLicense() {
+
+    return modelFactory.newRawLicense();
+  }
+
+  /**
+   * Checks if the given first String matches the second String. By default this is done with simple string comparison.
+   * There are some keywords which allow for advanced logic. If the seconds argument starts with "NOT:" then the rest of
+   * the condition is inverted. The prefix "REGEX:" and the suffix "(REGEX)" indicate that the remainder should be
+   * interpreted as a Java RegEx and matching will be done against this RegEx. "RANGE:" indicates that the following
+   * should be interpreted as a Maven version range expression.
+   *
+   * @param input the string to test
+   * @param condition the condition to test against
+   * @return <code>true</code> if it matches, <code>false</code> otherwise
+   */
+  public static boolean match(String input, String condition) {
+
+    if (input == null && condition == null) {
+      return true;
     }
-
-    /**
-     * Creates a new {@link Application} by calling
-     * {@link ModelFactory#newApplication(String, String, String, String, String)}.
-     *
-     * @param name the application name
-     * @param releaseId the release id.
-     * @param releaseDate the date of the release
-     * @param sourceRepo pointer to the source repo
-     * @param programmingEcosystem name of the programming ecosystem
-     * @return the new instance
-     */
-    public static Application newApplication(String name, String releaseId, String releaseDate, String sourceRepo,
-            String programmingEcosystem) {
-
-        return modelFactory.newApplication(name, releaseId, releaseDate, sourceRepo, programmingEcosystem);
+    if (condition != null) {
+      if (condition.startsWith(NOT_PREFIX)) {
+        String negatedCondition = condition.substring(NOT_PREFIX.length());
+        return !match(input, negatedCondition);
+      }
     }
-
-    /**
-     * Creates a new {@link ApplicationComponent} by calling
-     * {@link ModelFactory#newApplicationComponent()}.
-     *
-     * @return the new instance
-     */
-    public static ApplicationComponent newApplicationComponent() {
-
-        return modelFactory.newApplicationComponent();
+    if (input != null && condition != null) {
+      if (condition.startsWith(REGEX_PREFIX)) {
+        deprecationChecker.check(true,
+            "Use of 'REGEX:' prefix notation is deprecated, use '(REGEX)' suffix instead. See https://github.com/devonfw/solicitor/issues/78");
+        String pattern = condition.substring(REGEX_PREFIX.length());
+        return input.matches(pattern);
+      }
+      if (condition.endsWith(REGEX_SUFFIX)) {
+        String pattern = condition.substring(0, condition.length() - REGEX_SUFFIX.length()).trim();
+        return input.matches(pattern);
+      }
+      if (condition.startsWith(RANGE_PREFIX)) {
+        String rangeSpec = condition.substring(RANGE_PREFIX.length());
+        return MavenVersionHelper.checkVersionRange(input, rangeSpec);
+      }
+      return input.equals(condition);
     }
+    return false;
+  }
 
-    /**
-     * Creates a new {@link Engagement} by calling
-     * {@link ModelFactory#newEngagement(String, EngagementType, String, GoToMarketModel)}
-     * .
-     *
-     * @param engagementName the name of the engagement
-     * @param engagementType the type of engagement
-     * @param clientName name of the client
-     * @param goToMarketModel the model how this goes to market
-     * @return the new instance
-     */
-    public static Engagement newEngagement(String engagementName, EngagementType engagementType, String clientName,
-            GoToMarketModel goToMarketModel) {
+  /**
+   * This method gets the field <code>currentRuleGroup</code>. This field contains the id of the current rule group
+   * being processed by the Drools Rule Engine.
+   *
+   * @return the field currentRuleGroup
+   */
+  public static String getCurrentRuleGroup() {
 
-        return modelFactory.newEngagement(engagementName, engagementType, clientName, goToMarketModel);
-    }
+    return currentRuleGroup;
+  }
 
-    /**
-     * Creates a {@link NormalizedLicense} by calling
-     * {@link ModelFactory#newNormalizedLicense(RawLicense)}.
-     *
-     * @param rawLicense {@link RawLicense} from which to copy data
-     * @return the new instance
-     */
-    public static NormalizedLicense newNormalizedLicense(RawLicense rawLicense) {
+  /**
+   * This method sets the field <code>currentRuleGroup</code>.
+   *
+   * @param currentRuleGroup the new value of the field currentRuleGroup
+   */
+  public static void setCurrentRuleGroup(String currentRuleGroup) {
 
-        return modelFactory.newNormalizedLicense(rawLicense);
-    }
-
-    /**
-     * Creates a new {@link RawLicense} by calling
-     * {@link ModelFactory#newRawLicense()} on the implementation instance.
-     *
-     * @return the new instance
-     */
-    public static RawLicense newRawLicense() {
-
-        return modelFactory.newRawLicense();
-    }
-
-    /**
-     * Checks if the given first String matches the second String. By default
-     * this is done with simple string comparison. There are some keywords which
-     * allow for advanced logic. If the seconds argument starts with "NOT:" then
-     * the rest of the condition is inverted. The prefix "REGEX:" and the suffix "(REGEX)" indicate that
-     * the remainder should be interpreted as a Java RegEx and matching will be
-     * done against this RegEx. "RANGE:" indicates that the following should be
-     * interpreted as a Maven version range expression.
-     *
-     * @param input the string to test
-     * @param condition the condition to test against
-     * @return <code>true</code> if it matches, <code>false</code> otherwise
-     */
-    public static boolean match(String input, String condition) {
-
-        if (input == null && condition == null) {
-            return true;
-        }
-        if (condition != null) {
-            if (condition.startsWith(NOT_PREFIX)) {
-                String negatedCondition = condition.substring(NOT_PREFIX.length());
-                return !match(input, negatedCondition);
-            }
-        }
-        if (input != null && condition != null) {
-            if (condition.startsWith(REGEX_PREFIX)) {
-            	deprecationChecker.check(true,
-                        "Use of 'REGEX:' prefix notation is deprecated, use '(REGEX)' suffix instead. See https://github.com/devonfw/solicitor/issues/78");
-                String pattern = condition.substring(REGEX_PREFIX.length());
-                return input.matches(pattern);
-            }
-            if (condition.endsWith(REGEX_SUFFIX)) {
-            	condition = condition.replaceAll(" ", "");
-                String pattern = condition.substring(0,condition.length()-REGEX_SUFFIX.length());
-                return input.matches(pattern);
-            }
-            if (condition.startsWith(RANGE_PREFIX)) {
-                String rangeSpec = condition.substring(RANGE_PREFIX.length());
-                return MavenVersionHelper.checkVersionRange(input, rangeSpec);
-            }
-            return input.equals(condition);
-        }
-        return false;
-    }
-
-    /**
-     * Private Constructor to avoid instantiation.
-     */
-    private ModelHelper() {
-
-    }
-
-    /**
-     * Saves a reference to the ModelFactory in a static variable to allow
-     * access to factory methods via static methods.
-     *
-     * @param modelFactory a {@link ModelFactory} object.
-     */
-    @Autowired
-    public void setModelFactory(ModelFactory modelFactory) {
-
-        ModelHelper.modelFactory = modelFactory;
-    }
-
-    /**
-     * This method gets the field <code>currentRuleGroup</code>. This field
-     * contains the id of the current rule group being processed by the Drools
-     * Rule Engine.
-     *
-     * @return the field currentRuleGroup
-     */
-    public static String getCurrentRuleGroup() {
-
-        return currentRuleGroup;
-    }
-
-    /**
-     * This method sets the field <code>currentRuleGroup</code>.
-     *
-     * @param currentRuleGroup the new value of the field currentRuleGroup
-     */
-    public static void setCurrentRuleGroup(String currentRuleGroup) {
-
-        ModelHelper.currentRuleGroup = currentRuleGroup;
-    }
+    ModelHelper.currentRuleGroup = currentRuleGroup;
+  }
 
 }
--- core/src/test/java/com/devonfw/tools/solicitor/ruleengine/drools/ModelHelperTest.java
+++ core/src/test/java/com/devonfw/tools/solicitor/ruleengine/drools/ModelHelperTest.java
@@ -15,52 +15,55 @@ import com.devonfw.tools.solicitor.common.DeprecationChecker;
  */
 public class ModelHelperTest {
 
-	
-    /**
-     * Test method for
-     * {@link com.devonfw.tools.solicitor.ruleengine.drools.ModelHelper#match(java.lang.String, java.lang.String)}.
-     */
-    @Test
-    public void testMatch() {
-    	ModelHelper.setDeprecationChecker(new DeprecationChecker() {
+  /**
+   * Test method for
+   * {@link com.devonfw.tools.solicitor.ruleengine.drools.ModelHelper#match(java.lang.String, java.lang.String)}.
+   */
+  @Test
+  public void testMatch() {
 
-            @Override
-            public void check(boolean warnOnly, String detailsString) {
-                // do nothing...
-            }
-        });
-        assertTrue(ModelHelper.match(null, null));
-        assertFalse(ModelHelper.match(null, ""));
-        assertFalse(ModelHelper.match("", null));
-        assertTrue(ModelHelper.match("", ""));
-        assertTrue(ModelHelper.match("abc", "abc"));
-        assertFalse(ModelHelper.match("abc", "a.c"));
-        assertTrue(ModelHelper.match("abc", "REGEX:abc"));
-        assertTrue(ModelHelper.match("abc", "REGEX:a.c"));
-        assertFalse(ModelHelper.match("abc", "REGEX:a."));
-        assertTrue(ModelHelper.match("abc", "REGEX:a.*"));
-        assertTrue(ModelHelper.match("abc", "abc(REGEX)"));
-        assertTrue(ModelHelper.match("abc", "a.c (REGEX)"));
-        assertFalse(ModelHelper.match("abc", "a. (REGEX)"));
-        assertTrue(ModelHelper.match("abc", "a.*(REGEX)"));
+    // need to initialize on instance level but test is done on static methods
+    ModelHelper instance = new ModelHelper();
+    instance.setDeprecationChecker(new DeprecationChecker() {
 
-        assertTrue(ModelHelper.match("1.0", "RANGE:1.0"));
-        assertTrue(ModelHelper.match("1.0.1", "RANGE:1.0"));
-        assertFalse(ModelHelper.match("1.0.1", "RANGE:[1.0]"));
-        assertTrue(ModelHelper.match("1.0.1", "RANGE:[1.0,1.1)"));
-        assertFalse(ModelHelper.match("1.1", "RANGE:[1.0,1.1)"));
+      @Override
+      public void check(boolean warnOnly, String detailsString) {
 
-        assertTrue(ModelHelper.match(null, "NOT:"));
-        assertFalse(ModelHelper.match("", "NOT:"));
-        assertFalse(ModelHelper.match("abc", "NOT:abc"));
-        assertFalse(ModelHelper.match("abc", "NOT:REGEX:abc"));
-        assertTrue(ModelHelper.match("abc", "NOT:REGEX:a."));
-        assertFalse(ModelHelper.match("abc", "NOT:abc(REGEX)"));
-        assertTrue(ModelHelper.match("abc", "NOT:a. (REGEX)"));
+        // do nothing...
+      }
+    });
+    assertTrue(ModelHelper.match(null, null));
+    assertFalse(ModelHelper.match(null, ""));
+    assertFalse(ModelHelper.match("", null));
+    assertTrue(ModelHelper.match("", ""));
+    assertTrue(ModelHelper.match("abc", "abc"));
+    assertFalse(ModelHelper.match("abc", "a.c"));
+    assertTrue(ModelHelper.match("abc", "REGEX:abc"));
+    assertTrue(ModelHelper.match("abc", "REGEX:a.c"));
+    assertFalse(ModelHelper.match("abc", "REGEX:a."));
+    assertTrue(ModelHelper.match("abc", "REGEX:a.*"));
+    assertTrue(ModelHelper.match("abc", "abc(REGEX)"));
+    assertTrue(ModelHelper.match("abc", "a.c (REGEX)"));
+    assertFalse(ModelHelper.match("abc", "a. (REGEX)"));
+    assertTrue(ModelHelper.match("abc", "a.*(REGEX)"));
 
-        assertFalse(ModelHelper.match("1.0", "NOT:RANGE:1.0"));
-        assertTrue(ModelHelper.match("1.0.1", "NOT:RANGE:[1.0]"));
+    assertTrue(ModelHelper.match("1.0", "RANGE:1.0"));
+    assertTrue(ModelHelper.match("1.0.1", "RANGE:1.0"));
+    assertFalse(ModelHelper.match("1.0.1", "RANGE:[1.0]"));
+    assertTrue(ModelHelper.match("1.0.1", "RANGE:[1.0,1.1)"));
+    assertFalse(ModelHelper.match("1.1", "RANGE:[1.0,1.1)"));
 
-    }
+    assertTrue(ModelHelper.match(null, "NOT:"));
+    assertFalse(ModelHelper.match("", "NOT:"));
+    assertFalse(ModelHelper.match("abc", "NOT:abc"));
+    assertFalse(ModelHelper.match("abc", "NOT:REGEX:abc"));
+    assertTrue(ModelHelper.match("abc", "NOT:REGEX:a."));
+    assertFalse(ModelHelper.match("abc", "NOT:abc(REGEX)"));
+    assertTrue(ModelHelper.match("abc", "NOT:a. (REGEX)"));
+
+    assertFalse(ModelHelper.match("1.0", "NOT:RANGE:1.0"));
+    assertTrue(ModelHelper.match("1.0.1", "NOT:RANGE:[1.0]"));
+
+  }
 
 }
