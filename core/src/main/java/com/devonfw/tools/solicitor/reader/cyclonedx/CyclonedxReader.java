fe27856861a05727ad172282eef2fe7e890f79a9
duph97 <dung.a.pham@capgemini.com>
2023-08-08 15:03:22 +0200

    CycloneDX Reader (#184)
    Co-authored-by: ohecker <8004361+ohecker@users.noreply.github.com>

---
+++ core/src/main/java/com/devonfw/tools/solicitor/reader/cyclonedx/CyclonedxReader.java
@@ -0,0 +1,154 @@
+package com.devonfw.tools.solicitor.reader.cyclonedx;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.devonfw.tools.solicitor.common.LogMessages;
+import com.devonfw.tools.solicitor.common.SolicitorRuntimeException;
+import com.devonfw.tools.solicitor.common.packageurl.SolicitorPackageURLException;
+import com.devonfw.tools.solicitor.common.packageurl.impl.DelegatingPackageURLHandlerImpl;
+import com.devonfw.tools.solicitor.model.inventory.ApplicationComponent;
+import com.devonfw.tools.solicitor.model.masterdata.Application;
+import com.devonfw.tools.solicitor.model.masterdata.UsagePattern;
+import com.devonfw.tools.solicitor.reader.AbstractReader;
+import com.devonfw.tools.solicitor.reader.Reader;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+/**
+ * A {@link Reader} which reads data produced by the <a href="https://github.com/CycloneDX/cdxgen">CDXGEN Tool</a>.
+ */
+@Component
+public class CyclonedxReader extends AbstractReader implements Reader {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CyclonedxReader.class);
+
+  /**
+   * The supported type of this {@link Reader}.
+   */
+  public static final String SUPPORTED_TYPE = "cyclonedx";
+
+  @Autowired
+  private DelegatingPackageURLHandlerImpl delegatingPackageURLHandler;
+
+  public void setDelegatingPackageURLHandler(DelegatingPackageURLHandlerImpl delegatingPackageURLHandler) {
+
+    this.delegatingPackageURLHandler = delegatingPackageURLHandler;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public Set<String> getSupportedTypes() {
+
+    return Collections.singleton(SUPPORTED_TYPE);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void readInventory(String type, String sourceUrl, Application application, UsagePattern usagePattern,
+      String repoType, Map<String, String> configuration) {
+
+    int componentCount = 0;
+    int licenseCount = 0;
+    InputStream is;
+    try {
+      is = this.inputStreamFactory.createInputStreamFor(sourceUrl);
+    } catch (IOException e1) {
+      throw new SolicitorRuntimeException("Could not open inventory source '" + sourceUrl + "' for reading", e1);
+    }
+    // According to tutorial https://github.com/FasterXML/jackson-databind/
+    ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    try {
+      JsonNode rootNode = mapper.readTree(is);
+
+      // Get all components in SBOM
+      JsonNode componentsNode = rootNode.get("components");
+      if (componentsNode != null) {
+        for (JsonNode componentNode : componentsNode) {
+
+          String groupId = componentNode.get("group").asText();
+          String artifactId = componentNode.get("name").asText();
+          String version = componentNode.get("version").asText();
+          String purl = componentNode.get("purl").asText();
+
+          // Fill appComponents
+          ApplicationComponent appComponent = getModelFactory().newApplicationComponent();
+          appComponent.setApplication(application);
+          componentCount++;
+
+          appComponent.setGroupId(groupId);
+          appComponent.setArtifactId(artifactId);
+          appComponent.setVersion(version);
+          appComponent.setUsagePattern(usagePattern);
+          appComponent.setRepoType(repoType);
+
+          // Fill purl
+          try {
+            // check if handler exists for the package type defined in purl
+            if (!this.delegatingPackageURLHandler.sourceDownloadUrlFor(purl).isEmpty()) {
+              appComponent.setPackageUrl(purl);
+            }
+          } catch (SolicitorPackageURLException ex) {
+            if (LOG.isDebugEnabled()) {
+              LOG.debug("Problem with PackageURL", ex);
+            }
+            LOG.warn(LogMessages.CYCLONEDX_UNSUPPORTED_PURL.msg(), purl);
+          }
+
+          // Fill license information
+          JsonNode licensesNode = componentNode.get("licenses"); // licenses
+
+          // Case if no licenses field exists
+          if (licensesNode == null) {
+            addRawLicense(appComponent, null, null, sourceUrl);
+          }
+          // Case if licenses field exists but is empty
+          else if (licensesNode != null && licensesNode.isEmpty()) {
+            addRawLicense(appComponent, null, null, sourceUrl);
+          }
+          // Case if licenses field exists and contains licenses
+          else if (licensesNode != null && licensesNode.isEmpty() == false) {
+            // Iterate over each "license" object within the "licenses" array
+            for (JsonNode licenseNode : licensesNode) {
+              // Declared License can be written either in "id" or "name" field. Prefer "id" as its written in SPDX
+              // format.
+              if (licenseNode.get("license").has("id")) {
+                if (licenseNode.get("license").has("url")) {
+                  licenseCount++;
+                  addRawLicense(appComponent, licenseNode.get("license").get("id").asText(),
+                      licenseNode.get("license").get("url").asText(), sourceUrl);
+                } else {
+                  licenseCount++;
+                  addRawLicense(appComponent, licenseNode.get("license").get("id").asText(), null, sourceUrl);
+                }
+              } else if (licenseNode.get("license").has("name")) {
+                if (licenseNode.get("license").has("url")) {
+                  licenseCount++;
+                  addRawLicense(appComponent, licenseNode.get("license").get("name").asText(),
+                      licenseNode.get("license").get("url").asText(), sourceUrl);
+                } else {
+                  licenseCount++;
+                  addRawLicense(appComponent, licenseNode.get("license").get("name").asText(), null, sourceUrl);
+                }
+              }
+            }
+          }
+        }
+      }
+      doLogging(sourceUrl, application, componentCount, licenseCount);
+    } catch (IOException e) {
+      throw new SolicitorRuntimeException("Could not read CycloneDx inventory source '" + sourceUrl + "'", e);
+    }
+
+  }
+
+}
---
+++ core/src/test/java/com/devonfw/tools/solicitor/reader/cyclonedx/CyclonedxReaderTests.java
@@ -0,0 +1,188 @@
+
+package com.devonfw.tools.solicitor.reader.cyclonedx;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.devonfw.tools.solicitor.common.FileInputStreamFactory;
+import com.devonfw.tools.solicitor.common.packageurl.SolicitorPackageURLException;
+import com.devonfw.tools.solicitor.common.packageurl.impl.DelegatingPackageURLHandlerImpl;
+import com.devonfw.tools.solicitor.model.ModelFactory;
+import com.devonfw.tools.solicitor.model.impl.ModelFactoryImpl;
+import com.devonfw.tools.solicitor.model.inventory.ApplicationComponent;
+import com.devonfw.tools.solicitor.model.inventory.RawLicense;
+import com.devonfw.tools.solicitor.model.masterdata.Application;
+import com.devonfw.tools.solicitor.model.masterdata.UsagePattern;
+
+/**
+ * This class contains JUnit test methods for the {@link CyclonedxReader} class.
+ */
+public class CyclonedxReaderTests {
+  private static final Logger LOG = LoggerFactory.getLogger(CyclonedxReaderTests.class);
+
+  CyclonedxReader cdxr = new CyclonedxReader();
+
+  ModelFactory modelFactory = new ModelFactoryImpl();
+
+  // Create Mock for DelegatingPackageURLHandlerImpl
+  DelegatingPackageURLHandlerImpl delegatingPurlHandler = Mockito.mock(DelegatingPackageURLHandlerImpl.class);
+
+  /**
+   * Test the {@link CyclonedxReader#readInventory()} method. Input file is an SBOM containing maven components. Mock
+   * the case, that a maven PurlHandler exists.
+   */
+  @Test
+  public void readMavenFileAndCheckSize() {
+
+    // Always return a non-empty String for maven purls
+    Mockito.when(this.delegatingPurlHandler.sourceDownloadUrlFor(Mockito.startsWith("pkg:maven/"))).thenReturn("foo");
+
+    Application application = this.modelFactory.newApplication("testApp", "0.0.0.TEST", "1.1.2111", "http://bla.com",
+        "Java8");
+    this.cdxr.setModelFactory(this.modelFactory);
+    this.cdxr.setInputStreamFactory(new FileInputStreamFactory());
+    this.cdxr.setDelegatingPackageURLHandler(this.delegatingPurlHandler);
+    this.cdxr.readInventory("maven", "src/test/resources/mavensbom.json", application, UsagePattern.DYNAMIC_LINKING,
+        "cyclonedx", null);
+    LOG.info(application.toString());
+
+    assertEquals(32, application.getApplicationComponents().size());
+
+    boolean found = false;
+    for (ApplicationComponent ap : application.getApplicationComponents()) {
+      if (ap.getGroupId().equals("org.springframework.boot") && //
+          ap.getArtifactId().equals("spring-boot-starter-logging") && //
+          ap.getVersion().equals("2.3.3.RELEASE")) {
+        found = true;
+        assertEquals("pkg:maven/org.springframework.boot/spring-boot-starter-logging@2.3.3.RELEASE?type=jar",
+            ap.getPackageUrl());
+        break;
+      }
+    }
+    assertTrue(found);
+  }
+
+  /**
+   * Test the {@link CyclonedxReader#readInventory()} method. Input file is an SBOM containing maven components. Mock
+   * the case, that a maven PurlHandler does not exist.
+   */
+  @Test
+  public void readMavenFileAndCheckSizeNegative() {
+
+    // Always throw exception for maven purls
+    Mockito.when(this.delegatingPurlHandler.sourceDownloadUrlFor(Mockito.startsWith("pkg:maven/"))).thenThrow(
+        new SolicitorPackageURLException("No applicable SingleKindPackageURLHandler found for type 'maven'"));
+
+    Application application = this.modelFactory.newApplication("testApp", "0.0.0.TEST", "1.1.2111", "http://bla.com",
+        "Java8");
+    this.cdxr.setModelFactory(this.modelFactory);
+    this.cdxr.setInputStreamFactory(new FileInputStreamFactory());
+    this.cdxr.setDelegatingPackageURLHandler(this.delegatingPurlHandler);
+    this.cdxr.readInventory("maven", "src/test/resources/mavensbom.json", application, UsagePattern.DYNAMIC_LINKING,
+        "cyclonedx", null);
+    LOG.info(application.toString());
+
+    assertEquals(32, application.getApplicationComponents().size());
+
+  }
+
+  /**
+   * Test the {@link CyclonedxReader#readInventory()} method. Input file is an SBOM containing maven components. Check
+   * one entry for correct content.
+   */
+  @Test
+  public void readMavenFileAndCheckSingleContentSize() {
+
+    // Always return a non-empty String for maven purls
+    Mockito.when(this.delegatingPurlHandler.sourceDownloadUrlFor(Mockito.startsWith("pkg:maven/"))).thenReturn("foo");
+
+    Application application = this.modelFactory.newApplication("testApp", "0.0.0.TEST", "1.1.2111", "http://bla.com",
+        "Java8");
+    this.cdxr.setModelFactory(this.modelFactory);
+    this.cdxr.setInputStreamFactory(new FileInputStreamFactory());
+    this.cdxr.setDelegatingPackageURLHandler(this.delegatingPurlHandler);
+    this.cdxr.readInventory("maven", "src/test/resources/mavensbom.json", application, UsagePattern.DYNAMIC_LINKING,
+        "someRepoType", null);
+    LOG.info(application.toString());
+
+    assertEquals(32, application.getApplicationComponents().size());
+
+    boolean found = false;
+    for (ApplicationComponent ap : application.getApplicationComponents()) {
+      if (ap.getGroupId().equals("ch.qos.logback") && //
+          ap.getArtifactId().equals("logback-classic") && //
+          ap.getVersion().equals("1.2.3")) {
+        found = true;
+        assertEquals(UsagePattern.DYNAMIC_LINKING, ap.getUsagePattern());
+        assertEquals("someRepoType", ap.getRepoType());
+        assertEquals("pkg:maven/ch.qos.logback/logback-classic@1.2.3?type=jar", ap.getPackageUrl());
+        assertEquals(4, ap.getRawLicenses().size());
+        boolean l1found = false;
+        boolean l2found = false;
+        boolean l3found = false;
+        boolean l4found = false;
+
+        for (RawLicense rl : ap.getRawLicenses()) {
+          if (rl.getDeclaredLicense().equals("EPL-1.0")) {
+            l1found = true;
+            assertNull(rl.getLicenseUrl());
+          }
+          if (rl.getDeclaredLicense().equals("EPL-2.0")) {
+            l2found = true;
+            assertEquals("some url", rl.getLicenseUrl());
+          }
+          if (rl.getDeclaredLicense().equals("someOtherLicense")) {
+            l3found = true;
+            assertNull(rl.getLicenseUrl());
+          }
+          if (rl.getDeclaredLicense().equals("GNU Lesser General Public License")) {
+            l4found = true;
+            assertEquals("http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html", rl.getLicenseUrl());
+
+          }
+        }
+        assertTrue(l1found && l2found && l3found && l4found);
+        break;
+      }
+    }
+    assertTrue(found);
+  }
+
+  /**
+   * Test the {@link CyclonedxReader#readInventory()} method. Input file is an SBOM containing npm components. Mock the
+   * case, that a npm PurlHandler exists.
+   */
+  @Test
+  public void readNpmFileAndCheckSize() {
+
+    // Always return a non-empty String for npm purls
+    Mockito.when(this.delegatingPurlHandler.sourceDownloadUrlFor(Mockito.startsWith("pkg:npm/"))).thenReturn("foo");
+
+    Application application = this.modelFactory.newApplication("testApp", "0.0.0.TEST", "1.1.2111", "http://bla.com",
+        "Angular");
+    this.cdxr.setModelFactory(this.modelFactory);
+    this.cdxr.setInputStreamFactory(new FileInputStreamFactory());
+    this.cdxr.setDelegatingPackageURLHandler(this.delegatingPurlHandler);
+    this.cdxr.readInventory("npm", "src/test/resources/npmsbom.json", application, UsagePattern.DYNAMIC_LINKING,
+        "cyclonedx", null);
+    LOG.info(application.toString());
+
+    assertEquals(74, application.getApplicationComponents().size());
+
+    boolean found = false;
+    for (ApplicationComponent ap : application.getApplicationComponents()) {
+      if (ap.getArtifactId().equals("async") && ap.getVersion().equals("3.2.4")) {
+        found = true;
+        assertEquals("pkg:npm/async@3.2.4", ap.getPackageUrl());
+        break;
+      }
+    }
+    assertTrue(found);
+  }
+}
