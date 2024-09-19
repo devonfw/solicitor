27cc7ba40deb870bfa868072c94b1be583c2aaa7
dungpham <dung.a.pham@capgemini.com>
2024-09-19 16:20:12 +0200

    Trim category string

--- core/src/main/resources/com/devonfw/tools/solicitor/sql/statistics.sql
+++ core/src/main/resources/com/devonfw/tools/solicitor/sql/statistics.sql
@@ -1,11 +1,11 @@
--- Selects the category "legal-evaluation" and counts occurrences of each value in the 'legalApproved' column.
-SELECT 'legal-evaluation' AS category, NVL(l."legalApproved", 'blank') AS value , COUNT(*) AS count
+-- Selects the category "legal-evaluation" and trims any whitespace
+SELECT TRIM('legal-evaluation') AS category, NVL(l."legalApproved", 'blank') AS value, COUNT(*) AS count
 FROM NormalizedLicense l
 GROUP BY NVL(l."legalApproved", 'blank')
 
 UNION ALL -- Combines the results of this query with the next, including duplicates
 
--- Selects the category "data-status" and counts occurrences of each value in the 'dataStatus' column.
-SELECT 'data-status' AS category, ac."dataStatus" AS value, COUNT(*) AS count
+-- Selects the category "data-status" and trims any whitespace
+SELECT TRIM('data-status') AS category, ac."dataStatus" AS value, COUNT(*) AS count
 FROM ApplicationComponent ac
 GROUP BY ac."dataStatus";
