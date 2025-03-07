f9903a651c24caafeb6bdb0e40a76277be49b3cf
ohecker <8004361+ohecker@users.noreply.github.com>
2025-03-07 11:53:53 +0100

    Upgrade SPDX-Library to 2.0.0-RC2

--- core/src/main/java/com/devonfw/tools/solicitor/componentinfo/scancode/ScancodeLicenseMappingInventoryProcessor.java
+++ core/src/main/java/com/devonfw/tools/solicitor/componentinfo/scancode/ScancodeLicenseMappingInventoryProcessor.java
@@ -2,7 +2,7 @@ package com.devonfw.tools.solicitor.componentinfo.scancode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.spdx.library.model.license.LicenseInfoFactory;
+import org.spdx.library.LicenseInfoFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
