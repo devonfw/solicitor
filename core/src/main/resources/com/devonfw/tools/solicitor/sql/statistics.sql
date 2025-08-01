c0e9c9565e2344f4319f9d84c1a38c7c623273e7
ohecker <8004361+ohecker@users.noreply.github.com>
2025-08-01 09:05:01 +0200

    Extend data provided by the statistics.sql

--- core/src/main/resources/com/devonfw/tools/solicitor/sql/statistics.sql
+++ core/src/main/resources/com/devonfw/tools/solicitor/sql/statistics.sql
@@ -1,23 +1,102 @@
--- Selects the category "legal-evaluation" and trims any whitespace
+----------- category "legal evaluation" -----------------
+
 SELECT TRIM('legal-evaluation') AS category, NVL(l."legalApproved", 'blank') AS value, COUNT(*) AS count
-FROM 
+FROM
 	APPLICATION a,
 	APPLICATIONCOMPONENT ac,
 	NORMALIZEDLICENSE l
 where
-	a."reportingGroups" LIKE '%#reportingGroup#%' AND 
+	a."reportingGroups" LIKE '%#reportingGroup#%' AND
 	a.ID_APPLICATION = ac.PARENT_APPLICATIONCOMPONENT AND
 	ac.ID_APPLICATIONCOMPONENT = l.PARENT_NORMALIZEDLICENSE
 GROUP BY NVL(l."legalApproved", 'blank')
 
 UNION ALL -- Combines the results of this query with the next, including duplicates
 
--- Selects the category "data-status" and trims any whitespace
-SELECT TRIM('data-status') AS category, ac."dataStatus" AS value, COUNT(*) AS count
+SELECT TRIM('legal-evaluation') AS category, TRIM('total') AS value, COUNT(*) AS count
+FROM
+  APPLICATION a,
+  APPLICATIONCOMPONENT ac,
+  NORMALIZEDLICENSE l
+where
+  a."reportingGroups" LIKE '%#reportingGroup#%' AND
+  a.ID_APPLICATION = ac.PARENT_APPLICATIONCOMPONENT AND
+  ac.ID_APPLICATIONCOMPONENT = l.PARENT_NORMALIZEDLICENSE
+
+----------- category "effective-normalized-license-type" -----------------
+
+UNION ALL -- Combines the results of this query with the next, including duplicates
+
+SELECT TRIM('effective-normalized-license-type') AS category, NVL(l."effectiveNormalizedLicenseType", 'blank') AS value, COUNT(*) AS count
+FROM
+  APPLICATION a,
+  APPLICATIONCOMPONENT ac,
+  NORMALIZEDLICENSE l
+where
+  a."reportingGroups" LIKE '%#reportingGroup#%' AND
+  a.ID_APPLICATION = ac.PARENT_APPLICATIONCOMPONENT AND
+  ac.ID_APPLICATIONCOMPONENT = l.PARENT_NORMALIZEDLICENSE
+GROUP BY NVL(l."effectiveNormalizedLicenseType", 'blank')
+
+UNION ALL -- Combines the results of this query with the next, including duplicates
+
+SELECT TRIM('effective-normalized-license-type') AS category, TRIM( 'total') AS value, COUNT(*) AS count
+FROM
+  APPLICATION a,
+  APPLICATIONCOMPONENT ac,
+  NORMALIZEDLICENSE l
+where
+  a."reportingGroups" LIKE '%#reportingGroup#%' AND
+  a.ID_APPLICATION = ac.PARENT_APPLICATIONCOMPONENT AND
+  ac.ID_APPLICATIONCOMPONENT = l.PARENT_NORMALIZEDLICENSE
+
+----------- category "license-compliance" -----------------
+
+UNION ALL -- Combines the results of this query with the next, including duplicates
+
+SELECT TRIM('license-compliance') AS category, NVL(l."licenseCompliance", 'blank') AS value, COUNT(*) AS count
+FROM
+  APPLICATION a,
+  APPLICATIONCOMPONENT ac,
+  NORMALIZEDLICENSE l
+where
+  a."reportingGroups" LIKE '%#reportingGroup#%' AND
+  a.ID_APPLICATION = ac.PARENT_APPLICATIONCOMPONENT AND
+  ac.ID_APPLICATIONCOMPONENT = l.PARENT_NORMALIZEDLICENSE
+GROUP BY NVL(l."licenseCompliance", 'blank')
+
+UNION ALL -- Combines the results of this query with the next, including duplicates
+
+SELECT TRIM('license-compliance') AS category, TRIM( 'total') AS value, COUNT(*) AS count
+FROM
+  APPLICATION a,
+  APPLICATIONCOMPONENT ac,
+  NORMALIZEDLICENSE l
+where
+  a."reportingGroups" LIKE '%#reportingGroup#%' AND
+  a.ID_APPLICATION = ac.PARENT_APPLICATIONCOMPONENT AND
+  ac.ID_APPLICATIONCOMPONENT = l.PARENT_NORMALIZEDLICENSE
+
+----------- category "data-status" -----------------
+
+UNION ALL -- Combines the results of this query with the next, including duplicates
+
+SELECT TRIM('data-status') AS category, NVL(ac."dataStatus", 'blank') AS value, COUNT(*) AS count
 FROM
 	APPLICATION a,
     APPLICATIONCOMPONENT ac
 where
-	a."reportingGroups" LIKE '%#reportingGroup#%' AND 
+	a."reportingGroups" LIKE '%#reportingGroup#%' AND
 	a.ID_APPLICATION = ac.PARENT_APPLICATIONCOMPONENT
-GROUP BY ac."dataStatus";
+GROUP BY NVL(ac."dataStatus", 'blank')
+
+UNION ALL -- Combines the results of this query with the next, including duplicates
+
+SELECT TRIM('data-status') AS category, TRIM('total') AS value, COUNT(*) AS count
+FROM
+  APPLICATION a,
+    APPLICATIONCOMPONENT ac
+where
+  a."reportingGroups" LIKE '%#reportingGroup#%' AND
+  a.ID_APPLICATION = ac.PARENT_APPLICATIONCOMPONENT
+
